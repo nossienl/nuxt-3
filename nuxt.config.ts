673a66d60f2aacd079a1a734d0738d248ae74dde
Niels Otterspoor <niels.otterspoor@gmail.com>
2022-06-08 16:50:15 +0200

    add normalize.css and add main + base.scss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
-})
+  sourcemap: false,
+  css: [
+    // SCSS file in the project
+    "~/assets/styles/main.scss",
+  ],
+});
